13cf5bf81cb67d3a1a10353a1dacf9aa0e5a77bb
ucmxxw@163.com <ucmxxw@163.com>
2020-12-26 18:14:06 +0800

    csv

--- csv.go
+++ csv.go
@@ -1,1 +1,49 @@
 package wen
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/gocarina/gocsv"
+)
+
+type Example struct {
+	Id   string `csv: "id"`
+	Name string `csv:"name"`
+}
+
+func CsvWrite(path string, data []interface{}) error {
+	dir := filepath.Dir(path)
+	if dir != "" {
+		err := os.MkdirAll(dir, os.ModePerm)
+		if err != nil {
+			return err
+		}
+	}
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return gocsv.MarshalFile(data, f)
+}
+
+func CsvRead(path string, i interface{}) error {
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return gocsv.UnmarshalFile(f, i)
+}
+
+// func CsvFind(path string, args map[string]string, r interface{}) error {
+// 	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, os.ModePerm)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer f.Close()
+// 	gocsv.UnmarshalToCallback(f, func(t interface{}) {
+
+// 	})
+// }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module wen
 
 go 1.14
+
+require (
+	github.com/gocarina/gocsv v0.0.0-20201208093247-67c824bc04d4 // indirect
+	github.com/thoas/go-funk v0.7.0 // indirect
+)
