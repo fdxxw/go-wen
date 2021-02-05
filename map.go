41edc5df227b9fe29af480fd7693949f83bf8740
xingxiaowen <xiaowen.xing@accuenergy.com.cn>
2021-02-05 09:48:27 +0800

    map

--- map.go
+++ map.go
@@ -5,20 +5,35 @@ import "strings"
 type Map map[string]interface{}
 
 func (m Map) M(s string) Map {
+	if !m.Have(s) {
+		return nil
+	}
 	return m[s].(map[string]interface{})
 }
 
 func (m Map) S(s string) string {
+	if !m.Have(s) {
+		return ""
+	}
 	return m[s].(string)
 }
 
 func (m Map) Int(s string) int {
+	if !m.Have(s) {
+		return 0
+	}
 	return m[s].(int)
 }
 func (m Map) Bool(s string) bool {
+	if !m.Have(s) {
+		return false
+	}
 	return m[s].(bool)
 }
 func (m Map) I(s string) interface{} {
+	if !m.Have(s) {
+		return nil
+	}
 	return m[s]
 }
 func (m Map) Have(s string) bool {
