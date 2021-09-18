94883e6c90a0fa87ad91dca646a826777ae3da1c
xingxiaowen <xiaowen.xing@accuenergy.com.cn>
2021-09-18 11:42:35 +0800

    aes ecb

--- encrypt.go
+++ encrypt.go
@@ -1,6 +1,7 @@
 package wen
 
 import (
+	"bytes"
 	"crypto"
 	"crypto/aes"
 	"crypto/cipher"
@@ -126,6 +127,16 @@ func DecryptAES(data, key []byte) []byte {
 
 	return plaintext
 }
+
+// func DecryptAESCBC(data, key []byte) ([]byte, error) {
+// 	var aesBlockDecrypt cipher.Block
+// 	aesBlockDecrypt, err := aes.NewCipher(key)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	// aesDecrypt := cipher.NewCBCDecrypter(aesBlockDecrypt)
+// 	return nil, nil
+// }
 
 // BASE64解码
 func DecryptBASE64(message string) ([]byte, error) {
@@ -312,4 +323,99 @@ func BcryptCompare(hashedPwd string, plainPwd string) bool {
 		return false
 	}
 	return true
+}
+
+func EncryptAESECBPKCS5(data []byte, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	// 加密
+	ecrypt := NewECBEncrypter(block)
+
+	src := PKCS5Padding(data, ecrypt.BlockSize())
+	out := make([]byte, len(src))
+	ecrypt.CryptBlocks(out, src)
+	return out, nil
+}
+func DecryptAESECBPKCS5(data []byte, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	ecb := NewECBDecrypter(block)
+	out := make([]byte, len(data))
+	ecb.CryptBlocks(out, data)
+	return PKCS5UnPadding(out), nil
+}
+
+func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+func PKCS5UnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
+}
+
+type ecb struct {
+	b         cipher.Block
+	blockSize int
+}
+
+func newECB(b cipher.Block) *ecb {
+	return &ecb{
+		b:         b,
+		blockSize: b.BlockSize(),
+	}
+}
+
+type ecbEncrypter ecb
+
+// NewECBEncrypter returns a BlockMode which encrypts in electronic code book
+// mode, using the given Block.
+func NewECBEncrypter(b cipher.Block) cipher.BlockMode {
+	return (*ecbEncrypter)(newECB(b))
+}
+
+func (x *ecbEncrypter) BlockSize() int { return x.blockSize }
+
+func (x *ecbEncrypter) CryptBlocks(dst, src []byte) {
+	if len(src)%x.blockSize != 0 {
+		panic("crypto/cipher: input not full blocks")
+	}
+	if len(dst) < len(src) {
+		panic("crypto/cipher: output smaller than input")
+	}
+	for len(src) > 0 {
+		x.b.Encrypt(dst, src[:x.blockSize])
+		src = src[x.blockSize:]
+		dst = dst[x.blockSize:]
+	}
+}
+
+type ecbDecrypter ecb
+
+// NewECBDecrypter returns a BlockMode which decrypts in electronic code book
+// mode, using the given Block.
+func NewECBDecrypter(b cipher.Block) cipher.BlockMode {
+	return (*ecbDecrypter)(newECB(b))
+}
+
+func (x *ecbDecrypter) BlockSize() int { return x.blockSize }
+
+func (x *ecbDecrypter) CryptBlocks(dst, src []byte) {
+	if len(src)%x.blockSize != 0 {
+		panic("crypto/cipher: input not full blocks")
+	}
+	if len(dst) < len(src) {
+		panic("crypto/cipher: output smaller than input")
+	}
+	for len(src) > 0 {
+		x.b.Decrypt(dst, src[:x.blockSize])
+		src = src[x.blockSize:]
+		dst = dst[x.blockSize:]
+	}
 }
--- encrypt_test.go
+++ encrypt_test.go
@@ -3,6 +3,7 @@ package wen
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"log"
 	"testing"
 )
 
@@ -12,15 +13,37 @@ func TestEncryptAES(t *testing.T) {
 		panic(err.Error())
 	}
 	a := hex.EncodeToString(bytes)
-	b, _ := hex.DecodeString(a)
+	b, _ := hex.DecodeString("ddea76f09505ce6849e73c61d7399dd8")
 	println(a)
 	println(hex.EncodeToString(b))
-	password := []byte("123456")
-	d := EncryptAES(password, bytes)
-	println(string(DecryptAES(d, bytes)))
+	password := []byte("wxfa922f34e3eea513")
+	d := EncryptAES(password, b)
+	log.Println(hex.EncodeToString(d))
+	println(string(DecryptAES(d, b)))
 }
 
 func TestBcryptCompare(t *testing.T) {
 	Bcrypt("1qaz@WSX")
 	BcryptCompare("$2a$10$mP01Fk0gbMqCPipoFF7WX.IWcbwljKrs11KA2Von008JDjdafO4ea", "1qaz@WSX")
+}
+
+func TestHexStringToBytes(t *testing.T) {
+	b, err := hex.DecodeString("8AA8573565BFF96BE67C32142B41D0872CC674B4EC1DB341A3260F10152FEBA8")
+	log.Println(b)
+	// key := []byte("46EBA22EF5204DD5B110A1F730513RQW")
+	key, err := hex.DecodeString("ddea76f09505ce6849e73c61d7399dd8")
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println((DecryptAESECBPKCS5(b, key)))
+
+	// 加密
+	// ecrypt := NewECBEncrypter(block)
+
+	// src := PKCS5Padding([]byte("wxfa922f34e3eea513"), ecrypt.BlockSize())
+	// out = make([]byte, len(src))
+	// ecrypt.CryptBlocks(out, src)
+	// log.Println(strings.ToUpper(hex.EncodeToString(out)))
+	//log.Println(string(DecryptAES(b, []byte("46EBA22EF5204DD5B110A1F730513RQW"))))
 }
